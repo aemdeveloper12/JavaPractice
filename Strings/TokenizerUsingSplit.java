6db3ce74ecf175c17db9589e376237647e0a519d
aemdeveloper12 <rupanjaliakula@gmail.com>
2025-08-16 22:53:20 +0530

    Encapsulation examples and String Tokenizer using split

---
+++ Strings/TokenizerUsingSplit.java
@@ -0,0 +1,11 @@
+import java.util.StringTokenizer;
+class TokenizerUsingSplit {
+    public static void main(String[] args) {
+        String s = "JAVA PYTHON SQL AI";
+        String[] parts=s.split("A");
+        
+        for(String token:parts){
+            System.out.println(token);
+        }
+    }
+}
